b40c9411872fe0641643f45b8562810b7295f62f
Shida <wonderfulofgame@gmail.com>
2025-09-01 23:49:46 +0700

    add: Adding checkout pages

--- resources/views/pages/booking/check-booking.blade.php
+++ resources/views/pages/booking/check-booking.blade.php
@@ -3,7 +3,7 @@
 @section('background')
     <div id="Background"
         class="absolute top-0 w-full h-[810px] bg-[linear-gradient(180deg,#85C8FF_0%,#D4D1FE_47.05%,#F3F6FD_100%)]">
-        <img src="assets/images/backgrounds/Jumbo Jet Sky (1) 1.png"
+        <img src="{{ asset('assets/images/backgrounds/Jumbo Jet Sky (1) 1.png') }}"
             class="absolute right-0 top-[147px] object-contain max-h-[481px]" alt="background image">
     </div>
 @endsection
